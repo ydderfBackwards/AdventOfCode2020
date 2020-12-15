a1ebc6a30010c18283e2890a997f0cf65f0323e9
ydderfBackwards <34340157+ydderfBackwards@users.noreply.github.com>
2020-12-15 10:44:00 +0100

    Add files via upload

---
+++ Inputs/Day13.cs
@@ -0,0 +1,30 @@
+namespace AdventOfCode2020.Inputs
+{
+    class Day13
+    {
+
+        public string testInput = @"939
+7,13,x,x,59,x,31,19";
+
+//         public string testInput = @"939
+// 17,x,13,19";
+
+//         public string testInput = @"939
+// 67,7,59,61";
+
+//         public string testInput = @"939
+// 67,x,7,59,61";
+
+//         public string testInput = @"939
+// 67,7,x,59,61";
+
+//         public string testInput = @"939
+// 1789,37,47,1889";
+     
+
+
+
+        public string input = @"1008833
+19,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,643,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,17,13,x,x,x,x,23,x,x,x,x,x,x,x,509,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,29";
+    }
+}
