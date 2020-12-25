715eced4f258c32d502f2b33dafb13abf1d828f1
ydderfBackwards <34340157+ydderfBackwards@users.noreply.github.com>
2020-12-25 16:45:03 +0100

    Add files via upload

---
+++ Inputs/Day25.cs
@@ -0,0 +1,13 @@
+namespace AdventOfCode2020.Inputs
+{
+    class Day25
+
+    {
+        public string testInput = @"5764801
+        17807724";
+
+
+        public string input = @"17607508
+15065270";
+    }
+}
