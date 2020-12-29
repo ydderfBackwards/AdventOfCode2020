ea317f4e91110994d88d685394dd1961c16ad116
ydderfBackwards <34340157+ydderfBackwards@users.noreply.github.com>
2020-12-29 19:48:45 +0100

    Add files via upload

--- Day19/Day19.cs
+++ Day19/Day19.cs
@@ -10,26 +10,143 @@ namespace AdventOfCode2020
     {
         public string SolvePart1(string input)
         {
-
             var (checkLines, rules, ruleZero, charA, charB) = ReadInput(input);
-            var rulesTotal = CreateRuleZeroList(rules, ruleZero, charA, charB);
-            var count = CountValidMessages(rulesTotal, checkLines);
 
-            Console.WriteLine("TODO: Check why real input doesn't give correct result....");
+            //Old slower solution
+            // var rulesTotal = CreateRuleZeroList(rules, ruleZero, charA, charB);
+            // var count = CountValidMessages(rulesTotal, checkLines);
+
+            //New faster solution, but not completely fair because we have hard coded rule 42 en 31 for rule zero 
+            var rules42 = FindRule(rules, 42, charA, charB);
+            var rules31 = FindRule(rules, 31, charA, charB);
+            var count = CountValidMessages_Part1(rules42, rules31, checkLines);
+            return count.ToString();
+
+        }
+
+        public string SolvePart2(string input)
+        {
+            var (checkLines, rules, ruleZero, charA, charB) = ReadInput(input);
+
+            var rules42 = FindRule(rules, 42, charA, charB);
+            var rules31 = FindRule(rules, 31, charA, charB);
+
+            var count = CountValidMessages_Part2(rules42, rules31, checkLines);
+
             return count.ToString();
         }
 
 
-
-        public string SolvePart2(string input)
+        public int CountValidMessages_Part1(List<string> rules42, List<string> rules31, string[] checkLines)
         {
-            //Convert input to array of integers.
-            string[] lines = input.Split(Environment.NewLine);
+            int count = 0;
 
+            int line42Length = rules42[0].Length; //Made the assumption that all rules have the same length
+            int line31Length = rules31[0].Length;
 
-            return "ERROR: No program";
+            foreach (string line in checkLines)
+            {
+                int count42 = 0;
+                int count31 = 0;
+                string checkLine = line;
+                while (checkLine.Length > 0)
+                {
+                    string partOfLine = checkLine[0..line42Length];
+                    if (rules42.Any(x => x.Equals(partOfLine)))
+                    {
+                        checkLine = checkLine[line42Length..checkLine.Length];
+                        count42++;
+                    }
+                    else
+                    {
+                        goto Check31;
+                    }
+                }
 
+            Check31:
+
+                while (checkLine.Length > 0)
+                {
+                    string partOfLine = checkLine[0..line31Length];
+                    if (rules31.Any(x => x.Equals(partOfLine)))
+                    {
+                        checkLine = checkLine[line31Length..checkLine.Length];
+                        count31++;
+                    }
+                    else
+                    {
+                        goto Done;
+                    }
+                }
+
+                //Rule 0 is: 8 11 ==> this is: 42 42 31 (because 8 = 42 and 11 = 42 31)
+                if (count42 == 2 && count31 == 1)
+                {
+                    count++;
+                }
+            Done:;
+
+            }
+
+            return count;
         }
+
+
+        public int CountValidMessages_Part2(List<string> rules42, List<string> rules31, string[] checkLines)
+        {
+            int count = 0;
+            int line42Length = rules42[0].Length; //Made the assumption that all rules have the same length
+            int line31Length = rules31[0].Length;
+
+            foreach (string line in checkLines)
+            {
+                int count42 = 0;
+                int count31 = 0;
+                string checkLine = line;
+                while (checkLine.Length > 0)
+                {
+                    string partOfLine = checkLine[0..line42Length];
+                    if (rules42.Any(x => x.Equals(partOfLine)))
+                    {
+                        checkLine = checkLine[line42Length..checkLine.Length];
+                        count42++;
+                    }
+                    else
+                    {
+                        goto Check31;
+                    }
+                }
+
+            Check31:
+
+                while (checkLine.Length > 0)
+                {
+                    string partOfLine = checkLine[0..line31Length];
+                    if (rules31.Any(x => x.Equals(partOfLine)))
+                    {
+                        checkLine = checkLine[line31Length..checkLine.Length];
+                        count31++;
+                    }
+                    else
+                    {
+                        goto Done;
+                    }
+                }
+
+               //Rule 0 is: 8 11 ==> this is: 42 42 31 (because 8 = 42 and 11 = 42 31) or a repetition of this eq. 42 42 42 31 31
+                //Both rules should be used at leased ones, and rule 42 more than rule 31
+                if (count42 > 0 && count31 > 0 && count42 > count31)
+                {
+                    count++;
+                }
+
+            Done:;
+
+            }
+
+            return count;
+        }
+
 
         public int CountValidMessages(List<string> rules, string[] checkLines)
         {
@@ -44,7 +161,7 @@ namespace AdventOfCode2020
             return count;
         }
 
-        public List<string> CreateRuleZeroList(int[,,] rules, string ruleZero, int charA, int charB)
+        public List<string> CreateRuleZeroList(int[,,] rules, string ruleZero, int charA, int charB) //Old slow solution
         {
             List<string>[] ruleZeroList = new List<string>[3];
             List<string> rulesTotal = new List<string>();
@@ -61,11 +178,8 @@ namespace AdventOfCode2020
             {
                 foreach (string rule1 in ruleZeroList[1])
                 {
-                    // foreach (string rule2 in ruleZeroList[2])
-                    // {
                     string rule = rule0 + rule1;// + rule2;
                     rulesTotal.Add(rule);
-                    // }
                 }
             }
             List<string> distinct = rulesTotal.Distinct().ToList();
@@ -93,19 +207,19 @@ namespace AdventOfCode2020
             {
                 if (rules[ruleNr, 0, 0] != 0)
                 {
-                    tmpList00 = FindRule(rules, rules[ruleNr, 0, 0], charA, charB); //a
+                    tmpList00 = FindRule(rules, rules[ruleNr, 0, 0], charA, charB); 
                 }
                 if (rules[ruleNr, 0, 1] != 0)
                 {
-                    tmpList01 = FindRule(rules, rules[ruleNr, 0, 1], charA, charB); //a
+                    tmpList01 = FindRule(rules, rules[ruleNr, 0, 1], charA, charB); 
                 }
                 if (rules[ruleNr, 1, 0] != 0)
                 {
-                    tmpList10 = FindRule(rules, rules[ruleNr, 1, 0], charA, charB); //b
+                    tmpList10 = FindRule(rules, rules[ruleNr, 1, 0], charA, charB); 
                 }
                 if (rules[ruleNr, 1, 1] != 0)
                 {
-                    tmpList11 = FindRule(rules, rules[ruleNr, 1, 1], charA, charB); //b
+                    tmpList11 = FindRule(rules, rules[ruleNr, 1, 1], charA, charB); 
                 }
 
                 foreach (string tmp0 in tmpList00)
@@ -142,8 +256,6 @@ namespace AdventOfCode2020
                     }
 
                 }
-
-
             }
 
             return ruleList;
@@ -213,7 +325,6 @@ namespace AdventOfCode2020
 
         public string GetInput(bool testInput)
         {
-
             var myInput = new Inputs.Day19();
             return (testInput) ? myInput.testInput : myInput.input;
         }
